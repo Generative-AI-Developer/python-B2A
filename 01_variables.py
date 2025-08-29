fb23d05847aa5f8a5a1a159bc82511c6c590fed3
Muhammad Asif <programmerasif@hotmail.com>
2025-08-29 12:23:07 +0500

    added

---
+++ 01_variables.py
@@ -0,0 +1,8 @@
+greeting = "Bye"
+print(greeting + 'Muhammad')
+print(greeting + 'Asif')
+print(greeting + 'Qasim')
+
+'''What are variables in Python?
+Variables are used to store data in a program. They act as containers for values that can be
+used and manipulated throughout the code.'''
