a1019447cdadd3f9dfaa216b2208e581fd6f763f
Muhammad Asif <programmerasif@hotmail.com>
2025-09-03 12:19:28 +0500

    constants added

---
+++ constants.py
@@ -0,0 +1,21 @@
+PI = 3.14
+VERSION = "1.0.0"
+
+print(PI)
+print(VERSION)
+
+
+'''
+What are constants in Python and how do you define them?
+
+Constants in Python are variables that are meant to remain 
+unchanged throughout the program. By convention, constants 
+are defined using uppercase letters with underscores 
+separating words. 
+For example:   
+MY_CONSTANT = 42
+
+
+
+
+'''
